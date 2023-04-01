5c52723a05c4565f3a963c0f907207a9dc402733
aj00786 <125802842+aj00786@users.noreply.github.com>
2023-04-01 23:08:08 +0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,53 @@
+# from machinetranslation import translator
+# from flask import Flask, render_template, request
+# import json
+# import machinetranslation
+
+# app = Flask("Web Translator")
+
+# @app.route("/englishToFrench")
+# def englishToFrench():
+#     textToTranslate = request.args.get('textToTranslate')
+#     # Write your code here
+#     return englishToFrench(textToTranslate)
+
+# @app.route("/frenchToEnglish")
+# def frenchToEnglish():
+#     textToTranslate = request.args.get('textToTranslate')
+#     # Write your code here
+#     return frenchToEnglish(textToTranslate)
+
+# @app.route("/")
+# def renderIndexPage():
+#     # Write the code to render template
+
+
+
+#     return render_template('index.html')
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080)
+
+from machinetranslation import translator
+from flask import Flask, render_template, request
+import json
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    translated_text=translator.english_to_french(textToTranslate)
+    return translated_text
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    translated_text=translator.french_to_english(textToTranslate)
+    return translated_text
+
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
